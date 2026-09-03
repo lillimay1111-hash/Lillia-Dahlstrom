0f03dfcd330cf1ff85bdc61113ab91baf71b49de
lillimay1111-hash <lillimay1111@gmail.com>
2026-09-03 15:17:36 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Create the navigation menu
+document.getElementById("navigation").innerHTML = `
+    <a href="about.html">About Me</a>
+    <a href="experience.html">Experience</a>
+    <a href="projects.html">Projects</a>
+    <a href="contact.html">Contact</a>
+`;
